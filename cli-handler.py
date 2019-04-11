d1acef2788ef7edfcc1fc106cf99257b4c3da7b7
sour4bh <lethaltranquillizer@gmail.com>
2019-04-12 03:19:26 +0530

    remove

--- cli-handler.py
+++ cli-handler.py
@@ -10,9 +10,10 @@ parser.add_argument('note', help='String to use as Note\'s Text')
 parser.add_argument('title', nargs='?', help='String to use as Note\'s Title') 
 parser.add_argument('-l', '--label', action='store', help='Label to assign the Note to')    # catch multiple lablels 
 parser.add_argument('-p', '--pinned', action='store_true', help='Pin note')
-parser.add_argument('--color', choices=[color for color in noteColors], help='Choose note color')
+parser.add_argument('--color', choices=[color for color in keep.noteColors], help='Choose note color')
 
 args = parser.parse_args()
+
 
 
 
