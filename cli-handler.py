902071f1ec6d9597aa9aa486e9ac39b4b098ab43
sour4bh <lethaltranquillizer@gmail.com>
2019-04-12 03:13:08 +0530

    arg parse: added args

--- cli-handler.py
+++ cli-handler.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-
+# # !/usr/bin/env python3
 import grabtoken
-import keep
 import exceptions
+import keep 
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('note', help='String to use as Note\'s Text')
+parser.add_argument('title', nargs='?', help='String to use as Note\'s Title') 
+parser.add_argument('-l', '--label', action='store', help='Label to assign the Note to')    # catch multiple lablels 
+parser.add_argument('-p', '--pinned', action='store_true', help='Pin note')
+parser.add_argument('--color', choices=[color for color in noteColors], help='Choose note color')
+
+args = parser.parse_args()
+
+
+
 
